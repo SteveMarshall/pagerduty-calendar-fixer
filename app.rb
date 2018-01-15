344ddf43343a2aa065af39b262841fd3ad49a1f3
Steve Marshall <steve@stevemarshall.com>
2018-01-15 10:19:06 +0000

    Parse the calendar feed
    In terms of output, this is a no-op, but this introduces the icalendar
    gem into the process. This allows us to traverse and manipulate the
    calendar easily between parsing and rendering.

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 require 'open-uri'
+require 'icalendar'
 require 'sinatra'
 
 get '/' do
   if params['pagerduty_feed']
-    original_feed = open(params['pagerduty_feed']).read
-    original_feed
+    original_feed = open(params['pagerduty_feed']).read.force_encoding('utf-8')
+    calendar = Icalendar::Calendar.parse(original_feed)
+
+    calendar.first.to_ical
   else
     erb :index
   end
