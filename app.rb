3f645a9a2b57f5fb4380b6745ceee635c71cee7b
Steve Marshall <steve@stevemarshall.com>
2018-01-15 09:53:39 +0000

    Return the requested calendar feed, unaltered
    Let's start by just getting the original feed and returning that. From
    here, we can start to manipulate it…

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
+require 'open-uri'
 require 'sinatra'
 
 get '/' do
-  erb :index
+  if params['pagerduty_feed']
+    original_feed = open(params['pagerduty_feed']).read
+    original_feed
+  else
+    erb :index
+  end
 end
