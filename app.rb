8003bb9c3644b4a05a6aa3ab15673d8a5d58036c
Steve Marshall <steve@stevemarshall.com>
2018-01-15 10:20:37 +0000

    Convert events to all-day events
    We want the events to register as all-day, so convert them to
    `Icalendar::Values::Date` objects (removing their time component). This
    is the special sauce, right here.

--- app.rb
+++ app.rb
@@ -8,6 +8,12 @@ get '/' do
     original_feed = open(params['pagerduty_feed']).read.force_encoding('utf-8')
     calendar = Icalendar::Calendar.parse(original_feed)
 
+    # Force DateTimes to be Dates
+    calendar.first.events.each do |event|
+      event.dtstart = Icalendar::Values::Date.new(event.dtstart)
+      event.dtend = Icalendar::Values::Date.new(event.dtend)
+    end
+
     calendar.first.to_ical
   else
     erb :index
